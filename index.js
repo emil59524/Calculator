44849b20b2b99d16b8662fc055bb5048dea47d1d
Emil <emil595@yahoo.com>
2018-06-14 17:33:15 +0300

    choose operator handled with if

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const readline = require('readline-sync');
 (function() {
     "use strict";
+    console.log("Welcome to the calculator: \n=============")
     console.log("Enter the first number: ");
     const first_operand = +readline.prompt();
     console.log("Enter the second number: ");
     const second_operand = +readline.prompt();
-    const answer = first_operand * second_operand;
+    let answer;
+    console.log("Enter the operator: ");
+    const operator = readline.prompt();
+    if (operator == "+") {
+        answer = first_operand + second_operand;
+    } else if (operator == "*") {
+        answer = first_operand * second_operand;
+    } else if (operator == "-") {
+        answer = first_operand - second_operand;
+    } else if (operator == "/") {
+        answer = first_operand / second_operand;
+    } else {
+        answer = "Invalid operator";
+    }
     console.log("The product of the numbers is " + answer);
 
 })();
