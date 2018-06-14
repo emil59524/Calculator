b3fa51faebf433ef29b2d0ef944ed14b1c6bbdf6
Emil <emil595@yahoo.com>
2018-06-14 04:55:34 +0300

    Multiplier3

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const readline = require('readline-sync');
 (function() {
     "use strict";
     console.log("Enter the first number: ");
-    var first_operand = +readline.prompt();
+    const first_operand = +readline.prompt();
     console.log("Enter the second number: ");
-    var second_operand = +readline.prompt();
-    var answer = first_operand * second_operand;
+    const second_operand = +readline.prompt();
+    const answer = first_operand * second_operand;
     console.log("The product of the numbers is " + answer);
 
 })();
