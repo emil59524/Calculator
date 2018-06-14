a52276e3fc5bec615dea80d4668bd7a180a4b85b
Emil <emil595@yahoo.com>
2018-06-14 04:50:48 +0300

    Multiplier

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('welcome to the calculator!');
+const readline = require('readline-sync');
+(function() {
+    "use strict";
+    console.log("Enter the first number");
+    var first_operand = +readline.prompt();
+    console.log("Enter the second number");
+    var second_operand = +readline.prompt();
+    console.log("The product of the numbers = " + (first_operand * second_operand));
+
+})();
