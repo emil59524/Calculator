e00952ddd2e15f36dc819a7bc9e1a672ba3a8b26
Emil <emil595@yahoo.com>
2018-06-14 17:25:17 +0300

    operators with switch

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const readline = require('readline-sync');
 (function() {
     "use strict";
+    console.log("Welcome to the calculator:\n===============")
     console.log("Enter the first number: ");
     const first_operand = +readline.prompt();
     console.log("Enter the second number: ");
     const second_operand = +readline.prompt();
-    const answer = first_operand * second_operand;
-    console.log("The product of the numbers is " + answer);
+    console.log("Enter the operator:");
+    const operator = readline.prompt();
+    let answer;
+    switch (operator) {
+        case "+":
+            answer = first_operand + second_operand;
+            break;
+        case "*":
+            answer = first_operand * second_operand;
+            break;
+        case "-":
+            answer = first_operand - second_operand;
+            break;
+        case "/":
+            answer = first_operand / second_operand;
+            break;
+        default:
+            answer = "Not a valid operator";
+            break;
+
+    }
+
+    console.log("The answer is: " + answer);
 
 })();
