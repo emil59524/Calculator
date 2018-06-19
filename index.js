2d72c7f1e6f858bb5f1cc14e8f27fc06efa7bbf1
Emil <emil595@yahoo.com>
2018-06-19 20:19:42 +0300

    refactor names to camel case

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 const readline = require('readline-sync');
 
-function get_number_prompt(prompt_message, error_message) {
-    if (error_message == undefined) {
-        error_message = "Invalid entry: ";
+function getNumberPrompt(promptMessage, errorMessage) {
+    if (errorMessage == undefined) {
+        errorMessage = "Invalid entry: ";
     }
-    console.log(prompt_message);
+    console.log(promptMessage);
     numberString = readline.prompt();
     number = +numberString;
     while (numberString == "" || isNaN(number)) {
-        console.log(error_message + ": " + prompt_message);
+        console.log(errorMessage + ": " + promptMessage);
         numberString = readline.prompt();
         number = +numberString;
     }
     return number;
 }
 
-function get_operator_prompt(prompt_message, error_message) {
-    if (error_message == undefined) {
-        error_message = "Invalid entry: ";
+function getOperatorPrompt(promptMessage, errorMessage) {
+    if (errorMessage == undefined) {
+        errorMessage = "Invalid entry: ";
     }
-    console.log(prompt_message);
+    console.log(promptMessage);
     operator = readline.prompt();
-    while (!is_valid_operator(operator)) {
-        console.log(error_message + ": " + prompt_message);
+    while (!isValidOperator(operator)) {
+        console.log(errorMessage + ": " + promptMessage);
         operator = readline.prompt();
     }
     return operator;
 }
 
-function do_operation(operator, first_operand, second_operand) {
+function doOperation(operator, firstOperand, secondOperand) {
     switch (operator) {
         case "+":
-            first_operand = first_operand + second_operand;
+            firstOperand = firstOperand + secondOperand;
             break;
         case "*":
-            first_operand = first_operand * second_operand;
+            firstOperand = firstOperand * secondOperand;
             break;
         case "-":
-            first_operand = first_operand - second_operand;
+            firstOperand = firstOperand - secondOperand;
             break;
         case "/":
-            first_operand = first_operand / second_operand;
+            firstOperand = firstOperand / secondOperand;
             break;
         default:
-            first_operand = "Not a valid operator";
+            firstOperand = "Not a valid operator";
             break;
 
     }
-    return first_operand;
+    return firstOperand;
 }
 
-function repeat_operation(operator, numbers) {
+function repeatOperation(operator, numbers) {
     "use strict";
     if (numbers.length < 2) {
         throw "repeat_operation must receive an array with the length >= 2";
@@ -58,24 +58,24 @@ function repeat_operation(operator, numbers) {
     let answer = numbers[0];
     let i = 0;
     while (++i < numbers.length) {
-        answer = do_operation(operator, answer, numbers[i]);
+        answer = doOperation(operator, answer, numbers[i]);
     }
     return answer;
 }
 
-function is_valid_operator(operator) {
+function isValidOperator(operator) {
     if (operator !== "+" && operator !== "*" && operator !== "-" && operator !== "/") {
         return false;
     }
     return true;
 }
 
-function get_numbers_prompt(numbers_length) {
+function getNumbersPrompt(numbersLength) {
     "use strict";
     let i = -1;
     let numbers = [];
-    while (++i < numbers_length) {
-        const number = get_number_prompt("Enter number " + (i + 1),
+    while (++i < numbersLength) {
+        const number = getNumberPrompt("Enter number " + (i + 1),
             "Enter a valid number: ");
         numbers[i] = number;
     }
@@ -86,19 +86,19 @@ function get_numbers_prompt(numbers_length) {
     "use strict";
     console.log("Welcome to the calculator:\n===============")
     while (true) {
-        operator = get_operator_prompt("Enter operator: ", "Enter a valid operator (+, *, -, /)");
+        operator = getOperatorPrompt("Enter operator: ", "Enter a valid operator (+, *, -, /)");
 
-        let numbers_length = get_number_prompt("Enter how many numbers do you want to " + operator
+        let numbersLength = getNumberPrompt("Enter how many numbers do you want to " + operator
             + "?", "Enter a number: ");
-        while (numbers_length < 2) {
+        while (numbersLength < 2) {
             console.log("You must enter at least 2");
-            numbers_length = get_number_prompt("Enter how many numbers do you want to "
+            numbersLength = getNumberPrompt("Enter how many numbers do you want to "
                 + operator + "?", "Enter a number: ");
         }
 
-        let numbers = get_numbers_prompt(numbers_length);
+        let numbers = getNumbersPrompt(numbersLength);
 
-        let answer = repeat_operation(operator, numbers);
+        let answer = repeatOperation(operator, numbers);
 
         console.log("The answer is: " + answer);
     }
